2a979edfcf984de4369972cecbfb9981e1ff192e
sausukei <ikenosousuke@icloud.com>
2022-03-10 16:27:54 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -19,7 +19,7 @@ serve((req) => {
             case "/api/asmd": // addition, subtraction, multiplication, division の頭文字
                 return apiFourArithmeticOperations(req);
             case "/api/reverse":
-                return apiReverse(req),console.log(req);
+                return apiReverse(req);
             case "/api/getjson":
                 return console.log(req);
         }
