17ed5ff77b8bc34dbda1ee82a833a9f485a43e34
jintajinta <jinta219@gmail.com>
2022-03-10 13:02:34 +0900

    inputを追加

--- server.ts
+++ server.ts
@@ -4,98 +4,119 @@ import { format } from "https://deno.land/std@0.127.0/datetime/mod.ts";
 
 console.log("Listening on http://localhost:8000");
 serve((req) => {
-    const url = new URL(req.url);
-    const pathname = url.pathname;
+  const url = new URL(req.url);
+  const pathname = url.pathname;
 
-    console.log("Request:", req.method, pathname);
+  console.log("Request:", req.method, pathname);
 
-    // /api/ で始まる場合、API サーバっぽく処理して返す
-    if (pathname.startsWith("/api/")) {
-        switch (pathname) {
-            case "/api/time":
-                return apiTime(req);
-            case "/api/asmd": // addition, subtraction, multiplication, division の頭文字
-                return apiFourArithmeticOperations(req);
-            case "/api/reverse":
-                return apiReverse(req);
-        }
+  // /api/ で始まる場合、API サーバっぽく処理して返す
+  if (pathname.startsWith("/api/")) {
+    switch (pathname) {
+      case "/api/time":
+        return apiTime(req);
+      case "/api/asmd": // addition, subtraction, multiplication, division の頭文字
+        return apiFourArithmeticOperations(req);
+      case "/api/reverse":
+        return apiReverse(req);
+      case "/api/input":
+        return apiinput(req);
     }
+  }
 
-    // pathname に対応する static フォルダのファイルを返す（いわゆるファイルサーバ機能）
-    // / → static/index.html
-    // /hoge → static/hoge/index.html
-    // /fuga.html → static/fuga.html
-    // /img/piyo.jpg → static/img/piyo.jpg
-    return serveDir(req, {
-        fsRoot: "static",
-        urlRoot: "",
-        showDirListing: true,
-        enableCors: true
-    });
+  // pathname に対応する static フォルダのファイルを返す（いわゆるファイルサーバ機能）
+  // / → static/index.html
+  // /hoge → static/hoge/index.html
+  // /fuga.html → static/fuga.html
+  // /img/piyo.jpg → static/img/piyo.jpg
+  return serveDir(req, {
+    fsRoot: "static",
+    urlRoot: "",
+    showDirListing: true,
+    enableCors: true,
+  });
 });
 
 // 従来の function を使った関数宣言
 // 現在の日時を返す API
 function apiTime(req: Request) {
-    return new Response(format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+  return new Response(format(new Date(), "yyyy-MM-dd HH:mm:ss"));
 }
 
 // アロー関数を使った関数宣言
 // クエリパラメータの x と y の四則演算の結果を JSON で返す API
 const apiFourArithmeticOperations = (req: Request) => {
-    const params = parseSearchParams(new URL(req.url));
-    const x = params.x;
-    const y = params.y;
+  const params = parseSearchParams(new URL(req.url));
+  const x = params.x;
+  const y = params.y;
 
-    let addition = 0;
-    let subtraction = 0;
-    let multiplication = 0;
-    let division = 0;
-    if (typeof x === "number" && typeof y === "number") {
-        addition = x + y;
-        subtraction = x - y;
-        multiplication = x * y;
-        division = x / y;
-    }
-    return createJsonResponse({ x, y, addition, subtraction, multiplication, division });
-}
+  let addition = 0;
+  let subtraction = 0;
+  let multiplication = 0;
+  let division = 0;
+  if (typeof x === "number" && typeof y === "number") {
+    addition = x + y;
+    subtraction = x - y;
+    multiplication = x * y;
+    division = x / y;
+  }
+  return createJsonResponse({
+    x,
+    y,
+    addition,
+    subtraction,
+    multiplication,
+    division,
+  });
+};
 
 // URL のクエリパラメータをパースする
 const parseSearchParams = (url: URL) => {
-    const params: Record<string, string | number | boolean> = {};
-    for (const p of url.searchParams) {
-        const n = p[0], v = p[1];
-        if (v === "")
-            params[n] = true;
-        else if (v === "true")
-            params[n] = true;
-        else if (v === "false")
-            params[n] = false;
-        else if (!isNaN(Number(v)))
-            params[n] = +v;
-        else
-            params[n] = v;
+  const params: Record<string, string | number | boolean> = {};
+  for (const p of url.searchParams) {
+    const n = p[0], v = p[1];
+    if (v === "") {
+      params[n] = true;
+    } else if (v === "true") {
+      params[n] = true;
+    } else if (v === "false") {
+      params[n] = false;
+    } else if (!isNaN(Number(v))) {
+      params[n] = +v;
+    } else {
+      params[n] = v;
     }
-    return params;
+  }
+  return params;
 };
 
 // JSON のレスポンスを生成する
-const createJsonResponse = (obj: any) => new Response(JSON.stringify(obj), {
+const createJsonResponse = (obj: any) =>
+  new Response(JSON.stringify(obj), {
     headers: {
-        "content-type": "application/json; charset=utf-8"
-    }
-});
+      "content-type": "application/json; charset=utf-8",
+    },
+  });
 
 // クライアントから送られてきた JSON の message の文字列を反転して返す API
 // curl -X POST -d '{ "message": "hello" }' http://localhost:8000/api/reverse
 // → {"message":"olleh"}
 const apiReverse = async (req: Request) => {
-    const json = (await req.json()) as ApiReversePayload;
-    const message = json.message;
-    const reversedMessage = message.split("").reverse().join("");
-    return createJsonResponse({ message: reversedMessage });
+  const json = (await req.json()) as ApiReversePayload;
+  const message = json.message;
+  const reversedMessage = message.split("").reverse().join("");
+  return createJsonResponse({ message: reversedMessage });
 };
 
 type ApiReversePayload = {
-    message: string;
+  message: string;
+};
+
+const apiinput = async (req: Request) => {
+  const json = (await req.json()) as ApiinputPayload;
+  const message = json.message;
+  return createJsonResponse({ message: message });
+};
+
+type ApiinputPayload = {
+  message: string;
 };
