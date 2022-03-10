4a5a50afabe880c80a82c7303c55c34a82b6d85d
sausukei <ikenosousuke@icloud.com>
2022-03-10 18:06:04 +0900

    Update server.ts

--- server.ts
+++ server.ts
@@ -4,9 +4,9 @@ import { format } from "https://deno.land/std@0.127.0/datetime/mod.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
 console.log("Listening on http://localhost:8000");
-console.log(Deno.env.get("DATABASE_URL"));
-const databaseUrl = Deno.env.get("DATABASE_URL")!;
 
+const databaseUrl = Deno.env.get("DATABASE_URL")!;
+console.log(databaseUrl);
 const pool = new postgres.Pool(databaseUrl, 3, true);
 /*
 // Connect to the database
